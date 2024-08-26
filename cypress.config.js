d61a4818e783294f82d135e7cfc9c5b1b98b65fe
JuanA <juan.arrieta@webops.com>
2024-08-25 22:12:48 -0500

    First configurations

---
+++ cypress.config.js
@@ -0,0 +1,29 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+module.exports = defineConfig({
+  projectId: "gkprdt",
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("file:preprocessor",
+      createBundler({
+        plugins: [createEsbuildPlugin.default(config)],
+      }));
+      preprocessor.addCucumberPreprocessorPlugin(on, config);
+      return config;
+    },
+	specPattern: "**/*.feature",
+  },
+  videosFolder: "cypress/reports/videos",
+  screenshotsFolder: "cypress/reports/screenshots",
+  reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+    },
+});
